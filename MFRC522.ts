2f5bf7c24f6bb0999abfb9007692d60fa0109d3d
cspanjian <58835905+cspanjian@users.noreply.github.com>
2023-08-13 11:39:59 +0800

    精简了程序，只保留了必要的内容

--- MFRC522.ts
+++ MFRC522.ts
@@ -1,18 +1,18 @@
 /**
   * MFRC522 Block
+  * 目前来看，读UUID是一直可以读取的，但是读里面的数据，读了一次，就无法读第二次了
+  * 所以，索性把这个插件简化，只有读取UUID的功能
   */
 //% color="#275C6B" weight=100 icon="\uf2bb" block="MFRC522 RFID"
 namespace MFRC522 {
     let COMMAND_I2C_ADDRESS = 0x28
     let Type2=0
-    const BlockAdr: number[] = [8, 9, 10]
     let TPrescalerReg = 0x2B
     let TxControlReg = 0x14
     let PICC_READ = 0x30
     let PICC_ANTICOLL = 0x93
     let PCD_RESETPHASE = 0x0F
     let temp = 0
-    let val = 0
     let uid: number[] = []
 
     let returnLen = 0
@@ -20,8 +20,6 @@ namespace MFRC522 {
     let status = 0
     let u = 0
     let ChkSerNum = 0
-    let returnBits:any = null
-    let recvData : number[]= []
     let PCD_IDLE = 0
     let d=0
 
@@ -39,7 +37,6 @@ namespace MFRC522 {
     let FIFODataReg = 0x09
     let FIFOLevelReg = 0x0A
     let ControlReg = 0x0C
-    let Key = [255, 255, 255, 255, 255, 255]
 
     function SetBits (reg: number, mask: number) {
         let tmp = I2C_Read(reg)
@@ -47,131 +44,22 @@ namespace MFRC522 {
     }
 
     function I2C_Write (reg: number, val: number) {
-    /*    pins.digitalWritePin(DigitalPin.P16, 0)
-        pins.spiWrite((reg << 1) & 0x7E)
-        pins.spiWrite(val)
-        pins.digitalWritePin(DigitalPin.P16, 1)*/
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = val
         pins.i2cWriteBuffer(COMMAND_I2C_ADDRESS, buf)
     }
 
-    function readFromCard ():string {
-        let [status, Type2] = Request(PICC_REQIDL)
-        if (status != 0) {
-            return null, null
-        }
-
-        [status, uid] = AvoidColl()
-
-        if (status != 0) {
-            return null, null
-        }
-
-        let id = getIDNum(uid)
-        TagSelect(uid)
-        status = Authent(PICC_AUTHENT1A, 11, Key, uid)
-        let data:NumberFormat.UInt8LE []= []
-        let text_read=''
-        let block: number[] = []
-        if (status == 0) {
-            for (let BlockNum of BlockAdr) {
-                block = ReadRFID(BlockNum)
-                if (block) {
-                    data = data.concat(block)
-                }
-            }
-            if (data) {
-                for (let c of data) {
-                text_read=text_read.concat(String.fromCharCode(c))
-                }
-            }
-        }
-        Crypto1Stop()
-        return text_read
-    }
-
     function I2C_Read (reg: number) {
-    /*    pins.digitalWritePin(DigitalPin.P16, 0)
-        pins.spiWrite(((reg<<1)& 0x7E)|0x80)
-        val = pins.spiWrite(0)
-        pins.digitalWritePin(DigitalPin.P16, 1)
-        return val
-        */
         pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, reg, NumberFormat.UInt8BE);
         let val = pins.i2cReadNumber(COMMAND_I2C_ADDRESS, NumberFormat.UInt8BE);
         return val;
-    }
-
-    function writeToCard (txt: string): number {
-        [status, Type2] = Request(PICC_REQIDL)
-
-        if (status != 0) {
-            return null, null
-        }
-        [status, uid] = AvoidColl()
-
-        if (status != 0) {
-            return null, null
-        }
-
-        let id=getIDNum(uid)
-        TagSelect(uid)
-        status=Authent(PICC_AUTHENT1A, 11, Key, uid)
-        ReadRFID(11)
-
-        if (status == 0) {
-            let data:NumberFormat.UInt8LE []= []
-            for (let i = 0; i < txt.length; i++){
-                data.push(txt.charCodeAt(i))
-            }
-
-            for (let j = txt.length; j<48;j++){
-                data.push(32)
-            }
-
-            let b = 0
-            for (let BlockNum2 of BlockAdr) {
-                WriteRFID(BlockNum2, data.slice((b*16), ((b+1)*16)))
-                b ++
-            }
-        }
-
-            Crypto1Stop()
-            serial.writeLine("Written to Card")
-            return id
-        }
-
-
-    function ReadRFID (blockAdr: number) {
-        recvData = []
-        recvData.push(PICC_READ)
-        recvData.push(blockAdr)
-        let pOut2=[]
-        pOut2= CRC_Calculation(recvData)
-        recvData.push(pOut2[0])
-        recvData.push(pOut2[1])
-        let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, recvData)
-
-        if (status != 0) {
-            serial.writeLine("Error while reading!")
-        }
-
-        if (returnData.length != 16) {
-            return null
-        }
-        else {
-            return returnData
-        }
     }
 
     function ClearBits (reg: number, mask: number) {
         let tmp = I2C_Read(reg)
         I2C_Write(reg, tmp & (~mask))
     }
-
-
 
     function Request (reqMode: number):[number, any] {
         let Type:number[] = []
@@ -215,31 +103,6 @@ namespace MFRC522 {
             }
         }
         return [status, returnData]
-    }
-
-    function Crypto1Stop () {
-        ClearBits(Status2Reg, 0x08)
-    }
-
-
-    function Authent (authMode: number, BlockAdr: number, Sectorkey: number[], SerNum: number[]) {
-        let buff: number[] = []
-        buff.push(authMode)
-        buff.push(BlockAdr)
-        for (let l=0; l <(Sectorkey.length);l++){
-            buff.push(Sectorkey[l])
-        }
-        for (let m=0;m<4;m++){
-            buff.push(SerNum[m])
-        }
-        [status, returnData, returnLen] = MFRC522_ToCard(PCD_AUTHENT, buff)
-            if (status != 0){
-            serial.writeLine("AUTH ERROR!")
-        }
-        if ((I2C_Read(Status2Reg) & 0x08)==0){
-            serial.writeLine("AUTH ERROR2!")
-        }
-        return status
     }
 
     function MFRC522_ToCard (command: number, sendData: number[]):[number, number[],number] {
@@ -319,82 +182,6 @@ namespace MFRC522 {
         return [status, returnData, returnLen]
     }
 
-    function TagSelect (SerNum: number[]) {
-        let buff: number[] = []
-        buff.push(0x93)
-        buff.push(0x70)
-        for (let r=0;r<5;r++){
-            buff.push(SerNum[r])
-        }
-
-        let pOut = CRC_Calculation(buff)
-        buff.push(pOut[0])
-        buff.push(pOut[1])
-        let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, buff)
-        if ((status == 0) && (returnLen == 0x18)){
-            return returnData[0]
-        }
-        else{
-            return 0
-        }
-    }
-
-    function CRC_Calculation (DataIn: number[]) {
-        ClearBits(DivIrqReg, 0x04)
-        SetBits(FIFOLevelReg, 0x80)
-        for ( let s=0;s<(DataIn.length);s++){
-            I2C_Write(FIFODataReg, DataIn[s])
-        }
-        I2C_Write(CommandReg, 0x03)
-        let t = 0xFF
-
-        while (true){
-            let v = I2C_Read(DivIrqReg)
-            t--
-            if (!(t != 0 && !(v & 0x04))){
-                break
-            }
-        }
-
-        let DataOut: number[] = []
-        DataOut.push(I2C_Read(0x22))
-        DataOut.push(I2C_Read(0x21))
-        return DataOut
-    }
-
-    function WriteRFID (blockAdr: number, writeData: number[]) {
-        let buff: number[] = []
-        let crc: number[] = []
-
-        buff.push(0xA0)
-        buff.push(blockAdr)
-        crc = CRC_Calculation(buff)
-        buff.push(crc[0])
-        buff.push(crc[1])
-        let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, buff)
-        if ((status != 0) || (returnLen!=4) || ((returnData[0]&0x0F) != 0x0A)){
-            status = 2
-            serial.writeLine("ERROR")
-        }
-
-        if (status == 0) {
-            let buff2 : number []= []
-            for (let w=0;w<16;w++){
-                buff2.push(writeData[w])
-            }
-            crc = CRC_Calculation(buff2)
-            buff2.push(crc[0])
-            buff2.push(crc[1])
-            let [status, returnData, returnLen] = MFRC522_ToCard(PCD_TRANSCEIVE, buff2)
-            if ((status!=0)||(returnLen!=4)||((returnData[0]&0x0F)!=0x0A)){
-                serial.writeLine("Error while writing")
-            }
-            else{
-                serial.writeLine("Data written")
-            }
-        }
-    }
-
     function getIDNum(uid: number[]){
         let a= 0
 
@@ -422,13 +209,9 @@ namespace MFRC522 {
     /*
      * Initial setup
      */
-    //% block="Initialize MFRC522 Module"
+    //% block="初始化RFID读卡器"
     //% weight=100
    export function Init() {
-   /*    pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13)
-       pins.spiFormat(8, 0)
-       pins.digitalWritePin(DigitalPin.P16, 1)
-*/
        // reset module
        I2C_Write(CommandReg, PCD_RESETPHASE)
 
@@ -443,10 +226,11 @@ namespace MFRC522 {
 
    /*
     * Function to read ID from card
+    * 这是一个阻塞式的函数，它会等在那边知道读到数据
     */
-   //% block="Read ID"
+   //% block="阻塞式读取RFID卡的ID"
    //% weight=95
-   export function getID() {
+   export function getIDBlocking() {
        let id = readID()
        while (!(id)) {
            id = readID()
@@ -458,52 +242,16 @@ namespace MFRC522 {
    }
 
     /*
-     * Function to read Data from card
+     * Function to read ID from card
+     * 这是一个非阻塞式的函数，读到数据，就返回ID；读不到，就返回0
      */
-    //% block="Read data"
+    //% block="非阻塞式读取RFID卡的ID"
     //% weight=90
-   export function read():string {
-       let text = readFromCard()
-       while (!text) {
-           let text = readFromCard()
-
-           if (text!=''){
-               return text
-           }
-       }
-       return text
-   }
-
-
-
-
-   /*
-    * Function to write Data
-    */
-   //% block="Write Data %text"
-   //% text
-   //% weight=85
-  export function write (text: string) {
-      let id = writeToCard(text)
-
-      while (!id) {
-          let id = writeToCard(text)
-
-          if (id != undefined){
-              return
-          }
-      }
-      return
-  }
-
-  /*
-   * TUrn off antenna
-   */
-  //% block="Turn off antenna"
-  //% text
-  //% weight=80
-  export function AntennaOff () {
-      ClearBits(TxControlReg, 0x03)
-  }
-
+    export function getIDNonBlocking() {
+        let id = readID()
+        if (id == undefined) {
+            return 0
+        }
+        return id
+    }
 }
